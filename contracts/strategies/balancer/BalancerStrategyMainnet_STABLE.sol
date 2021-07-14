6a7217cb79b268505ecac59efe938446507846f0
JasperSlimmens <jasper@slimmens.nl>
2021-07-14 16:48:55 +0200

    Add stable pool

--- contracts/strategies/balancer/BalancerStrategyMainnet_STABLE.sol
+++ contracts/strategies/balancer/BalancerStrategyMainnet_STABLE.sol
@@ -2,9 +2,9 @@
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
-import "./BalancerStrategy5Token.sol";
+import "./BalancerStrategy4Token.sol";
 
-contract BalancerStrategyMainnet_QIPOOL is BalancerStrategy5Token {
+contract BalancerStrategyMainnet_STABLE is BalancerStrategy4Token {
 
   constructor() public {}
 
@@ -12,30 +12,27 @@ contract BalancerStrategyMainnet_QIPOOL is BalancerStrategy5Token {
     address _storage,
     address _vault
   ) public initializer {
-    address underlying = address(0xf461f2240B66D55Dcf9059e26C022160C06863BF);
-    address wmatic = address(0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270);
+    address underlying = address(0x06Df3b2bbB68adc8B0e302443692037ED9f91b42);
+    address usdt = address(0xc2132D05D31c914a87C6611C10748AEb04B58e8F);
     address usdc = address(0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174);
-    address qi = address(0x580A84C73811E1839F75d86d75d88cCa0c241fF4);
-    address bal = address(0x9a71012B13CA4d3D0Cdc72A177DF3ef03b0E76A3);
     address mimatic = address(0xa3Fa99A148fA48D14Ed51d610c367C61876997F1);
-    BalancerStrategy5Token.initializeBaseStrategy(
+    address dai = address(0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063);
+    BalancerStrategy4Token.initializeBaseStrategy(
       _storage,
       underlying,
       _vault,
       address(0xBA12222222228d8Ba445958a75a0704d566BF2C8), //balancer vault
-      0xf461f2240b66d55dcf9059e26c022160c06863bf000100000000000000000006,  // Pool id
+      0x06df3b2bbb68adc8b0e302443692037ed9f91b42000000000000000000000012,  // Pool id
       500,    //Liquidation ratio, liquidate 50% on doHardWork
       usdc,   //depositToken
-      1,      //depositArrayIndex
+      0,      //depositArrayIndex
       0x0297e37f1873d2dab4487aa67cd56b58e2f27875000100000000000000000002 //bal2weth pid
     );
-    poolAssets = [wmatic, usdc, qi, bal, mimatic];
+    poolAssets = [usdc, dai, mimatic, usdt];
     WETH2deposit = [weth, usdc];
     reward2WETH[wmatic] = [wmatic, weth];
-    reward2WETH[qi] = [qi, mimatic, usdc, weth];
-    rewardTokens = [bal, wmatic, qi];
+    rewardTokens = [bal, wmatic];
     useQuick[wmatic] = true;
-    useQuick[qi] = true;
     useQuick[usdc] = true;
   }
 }
---
+++ test/balancer/stable.js
@@ -0,0 +1,131 @@
+// Utilities
+const Utils = require("../utilities/Utils.js");
+const {
+  impersonates,
+  setupCoreProtocol,
+  depositVault,
+  swapMaticToToken,
+  addLiquidity,
+  wrapMATIC
+} = require("../utilities/hh-utils.js");
+
+const addresses = require("../test-config.js");
+const { send } = require("@openzeppelin/test-helpers");
+const BigNumber = require("bignumber.js");
+const IERC20 = artifacts.require("IERC20");
+
+//const Strategy = artifacts.require("");
+const Strategy = artifacts.require("BalancerStrategyMainnet_STABLE");
+
+// Developed and tested at blockNumber 16812550
+
+// Vanilla Mocha test. Increased compatibility with tools that integrate Mocha.
+describe("Polygon Mainnet Balancer Stable", function() {
+  let accounts;
+
+  // external contracts
+  let underlying;
+
+  // external setup
+  let underlyingWhale = "0x89b24920E787ea34a661Ea1288adda53F658839A";
+  let balHolder = "0xc79dF9fe252Ac55AF8aECc3D93D20b6A4A84527B";
+  let balAddr = "0x9a71012B13CA4d3D0Cdc72A177DF3ef03b0E76A3";
+
+  // parties in the protocol
+  let governance;
+  let farmer1;
+
+  // numbers used in tests
+  let farmerBalance;
+
+  // Core protocol contracts
+  let controller;
+  let vault;
+  let strategy;
+
+  async function setupExternalContracts() {
+    underlying = await IERC20.at("0x06Df3b2bbB68adc8B0e302443692037ED9f91b42");
+    bal = await IERC20.at(balAddr);
+    console.log("Fetching Underlying at: ", underlying.address);
+  }
+
+  async function setupBalance(){
+    let etherGiver = accounts[9];
+    await send.ether(etherGiver, underlyingWhale, "1" + "000000000000000000");
+
+    farmerBalance = await underlying.balanceOf(underlyingWhale);
+    await underlying.transfer(farmer1, farmerBalance, { from: underlyingWhale });
+  }
+
+  before(async function() {
+    governance = "0xf00dD244228F51547f0563e60bCa65a30FBF5f7f";
+    accounts = await web3.eth.getAccounts();
+
+    farmer1 = accounts[1];
+
+    // impersonate accounts
+    await impersonates([governance, underlyingWhale, balHolder]);
+
+    let etherGiver = accounts[9];
+    await send.ether(etherGiver, governance, "100" + "000000000000000000");
+    await send.ether(etherGiver, balHolder, "100" + "000000000000000000");
+
+    await setupExternalContracts();
+    [controller, vault, strategy] = await setupCoreProtocol({
+      "existingVaultAddress": null,
+      "strategyArtifact": Strategy,
+      "strategyArtifactIsUpgradable": true,
+      "underlying": underlying,
+      "governance": governance,
+    });
+
+    await strategy.setSellFloor(1, {from:governance});
+
+    // whale send underlying to farmers
+    await setupBalance();
+  });
+
+  describe("Happy path", function() {
+    it("Farmer should earn money", async function() {
+      let farmerOldBalance = new BigNumber(await underlying.balanceOf(farmer1));
+      await depositVault(farmer1, underlying, vault, farmerBalance);
+
+      // Using half days is to simulate how we doHardwork in the real world
+      // The rewards are time based and Hardhat propagates the chain with a block time of 16s
+      // So we have 225 blocks per hour, 2700 blocks per 12 hours.
+      let hours = 10;
+      let blocksPerHour = 2700;
+      let oldSharePrice;
+      let newSharePrice;
+      for (let i = 0; i < hours; i++) {
+        console.log("loop ", i);
+
+        oldSharePrice = new BigNumber(await vault.getPricePerFullShare());
+        await controller.doHardWork(vault.address, { from: governance });
+        newSharePrice = new BigNumber(await vault.getPricePerFullShare());
+
+        console.log("old shareprice: ", oldSharePrice.toFixed());
+        console.log("new shareprice: ", newSharePrice.toFixed());
+        console.log("growth: ", newSharePrice.toFixed() / oldSharePrice.toFixed());
+
+        await bal.transfer(strategy.address, "100" + "000000000000000000", {from: balHolder});
+        await send.ether(accounts[9], strategy.address, "100" + "000000000000000000");
+
+        await Utils.advanceNBlock(blocksPerHour);
+      }
+      await vault.withdraw(new BigNumber(await vault.balanceOf(farmer1)).toFixed(), { from: farmer1 });
+      let farmerNewBalance = new BigNumber(await underlying.balanceOf(farmer1));
+      Utils.assertBNGt(farmerNewBalance, farmerOldBalance);
+
+      apr = (farmerNewBalance.toFixed()/farmerOldBalance.toFixed()-1)*(24/(blocksPerHour*hours/225))*365;
+      apy = ((farmerNewBalance.toFixed()/farmerOldBalance.toFixed()-1)*(24/(blocksPerHour*hours/225))+1)**365;
+
+      console.log("earned!");
+      console.log("APR:", apr*100, "%");
+      console.log("APY:", (apy-1)*100, "%");
+
+      await strategy.withdrawAllToVault({from:governance}); // making sure can withdraw all for a next switch
+
+    });
+  });
+});
